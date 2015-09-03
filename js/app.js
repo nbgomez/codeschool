afd6ce3be641035afabb3188edf67b1a3cc4f64e
Nick Gomez <ngomez@itnes.com>
2015-09-03 07:04:39 -0600

    Almost done with 2.6 of code school angular tutorial.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-var app = angular.module('store',[] );
+(function(){ 
+	var app = angular.module('store',[] );
+	
+	app.controller('StoreController', function(){
+		this.products=gems;
+	});
+	
+	app.controller('PanelController', function(){
+		this.tab= 1;
+		
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function (checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+	
+	var gems= [
+		{
+			name:"dod",
+			price:2.95,
+			description:'...',
+			canPurchase:true,
+			soldOut:false
+		},
+		{
+			name:"DOD",
+			price:2.,
+			description:'.......',
+			canPurchase:true,
+			soldOut:false,	
+		}
+	]
+
+})();
